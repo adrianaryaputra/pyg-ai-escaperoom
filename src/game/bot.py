696ec63d01fd39ff063a4ccfcc2b734119009bfe
juliussin <julius.sintara@gmail.com>
2022-05-22 02:14:43 +0800

    make new classese for population and individual

--- src/game/bot.py
+++ src/game/bot.py
@@ -153,3 +153,115 @@ class Individual(pygame.sprite.Sprite):
                 self.rect.top = block[0].rect.bottom
             elif block[1].width < block[1].height:
                 self.rect.left = block[0].rect.right
+
+
+class Population():
+    def __init__(self, num_individual: int, max_speed: int = 5, individual_type: pygame.sprite.Sprite = Individual ) -> None:
+        self.initial_position = (400, 300)
+        self.num_instruction = 100
+        self.num_individual = num_individual
+        self.max_speed = max_speed
+        self.instructions = self._createInstructions()
+        self.steps = np.zeros(num_individual)
+        self.individuals = []
+        for n in range(self.num_individual):
+            self.individuals.append(xxxIndividual(self.initial_position, 20, self.instructions[n], self.max_speed))
+
+    def _createInstructions(self):
+        instructions = []
+        for n in range(self.num_instruction):
+            instructions.append(self._randomAngle)
+        return np.array(instructions)
+
+    def _randomAngle(self, seed: int = None):
+        if seed is not None:
+            np.random.seed(seed)
+        val = np.random.rand(self.num_instruction) * 2 * math.pi
+        directions = np.stack((np.cos(val)*self.max_speed, np.sin(val)*self.max_speed), axis=1)
+        directions = directions.astype(int)
+        return directions
+
+    def updateInstructions(self) -> None:
+        for n in range(self.num_individual):
+            self.individuals[n].instructions = self.instructions[n]
+
+    def updateFitness(self, finish_point: tuple[int, int]) -> None:
+        for n in range(self.num_individual):
+            self.individuals[n].calculateFitness(finish_point)
+    
+    def resetSteps(self) -> None:
+        self.steps = np.zeros(self.num_individual)
+
+    def allDead(self) -> bool:
+        for n in range(self.num_individual):
+            if self.individuals[n].is_alive:
+                return False
+        return True
+        
+
+
+class xxxIndividual(pygame.sprite.Sprite):
+    def __init__(self, position: tuple[int, int], size: int, instructions, max_speed: int) -> None:
+        super().__init__()
+        # self.position = position
+        self.size = size
+        self.velocity = np.array((0, 0))
+        self.instructions = instructions
+        self.max_speed = max_speed
+        # create an object for representing image
+        self.image = pygame.Surface(size=(size, size))
+        # set the location of the object
+        self.rect = self.image.get_rect(center=position)
+        self.image.fill(COLOR.PLAYER)
+        self.is_alive = True
+        self.fitness = 0
+        self.step = 0
+
+    def calculateFitness(self, finish_point: tuple[int, int]) -> None:
+        diff = ((self.rect.centerx - finish_point[0]), (self.rect.centery - finish_point[1]))
+        eucdistance = math.sqrt(diff[0]**2 + diff[1]**2)
+        self.fitness = 1.0 / (eucdistance**2)
+
+    def update(self, parent):
+        if not self.is_alive:
+            return
+        if len(self.instructions) > self.step:
+            self.velocity += self.instructions[self.step]
+            self.velocity.clip(-self.max_speed, self.max_speed)
+            self.step += 1
+        else:
+            self.velocity = np.array([0, 0])
+        # self.position += self.velocity
+        self.rect.move_ip(self.velocity)
+        self.collisionHandler(parent)
+
+    def draw(self, parent):
+        parent.image.blit(self.image, self.rect)
+
+    def collisionHandler(self, parent):
+        # transform parent rect to relative coordinate
+        rect = parent.rect.copy()
+        rect.move_ip(-parent.rect.left, -parent.rect.top)
+        
+        # Check if player touch the edge -> Die
+        if not rect.collidepoint(self.rect.topleft) or not rect.collidepoint(self.rect.bottomright):
+            self.is_alive = False
+
+        # peg player inside map surface
+        self.rect.clamp_ip(rect)
+
+        # check collision with all blocks
+        collidableBlock = [block for block in parent.blocks if block.collidable]
+        collisionListID = self.rect.collidelistall(collidableBlock)
+        collisionList = [collidableBlock[i] for i in collisionListID]
+        collisionClip = [self.rect.clip(coll) for coll in collisionList]
+
+        for block in zip(collisionList, collisionClip):
+            if block[0].rect.x == block[1].x and block[1].width < block[1].height:
+                self.rect.right = block[0].rect.left
+            elif block[0].rect.y == block[1].y and block[1].width > block[1].height:
+                self.rect.bottom = block[0].rect.top
+            elif block[1].width > block[1].height:
+                self.rect.top = block[0].rect.bottom
+            elif block[1].width < block[1].height:
+                self.rect.left = block[0].rect.right
